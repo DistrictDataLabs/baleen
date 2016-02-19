668b5fdc8440dcee29115b6e08fe4e880c1a7e65
Benjamin Bengfort <benjamin@bengfort.com>
2016-02-18 20:22:39 -0500

    pypi packaging requirements

--- setup.py
+++ setup.py
@@ -1,45 +1,50 @@
 #!/usr/bin/env python
 # setup
-# Setup script for baleen
+# Setup script for installing baleen
 #
 # Author:   Benjamin Bengfort <benjamin@bengfort.com>
 # Created:  Fri Sep 19 10:59:24 2014 -0400
 #
-# Copyright (C) 2014 District Data Labs
+# Copyright (C) 2014 Bengfort.com
 # For license information, see LICENSE.txt and NOTICE.md
 #
 # ID: setup.py [] benjamin@bengfort.com $
 
 """
-Setup script for baleen
+Setup script for installing baleen.
+See http://bbengfort.github.io/programmer/2016/01/20/packaging-with-pypi.html
 """
 
 ##########################################################################
 ## Imports
 ##########################################################################
 
-try:
-    from setuptools import setup
-    from setuptools import find_packages
-except ImportError:
-    raise ImportError("Could not import \"setuptools\"."
-                      "Please install the setuptools package.")
+import os
+import re
+import codecs
+
+from setuptools import setup
+from setuptools import find_packages
 
 ##########################################################################
 ## Package Information
 ##########################################################################
 
-## Discover the packages
-packages = find_packages(where=".", exclude=("tests", "bin", "docs", "fixtures",))
+## Basic information
+NAME         = "baleen"
+DESCRIPTION  = "An automated ingestion service for blogs to construct a corpus for NLP research."
+AUTHOR       = "Benjamin Bengfort"
+EMAIL        = "benjamin@bengfort.com"
+LICENSE      = "MIT"
+REPOSITORY   = "https://github.com/bbengfort/baleen"
+PACKAGE      = "baleen"
 
-## Load the requirements
-requires = []
-with open('requirements.txt', 'r') as reqfile:
-    for line in reqfile:
-        requires.append(line.strip())
+## Define the keywords
+KEYWORDS     = ('nlp', 'baleen', 'ingestion', 'blogs', 'rss')
 
 ## Define the classifiers
-classifiers = (
+## See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+CLASSIFIERS  = (
     'Development Status :: 4 - Beta',
     'Environment :: Console',
     'Intended Audience :: Developers',
@@ -53,24 +58,71 @@ classifiers = (
     'Topic :: Utilities',
 )
 
-## Define the keywords
-keywords = ('nlp', 'baleen', 'ingestion', 'blogs', 'rss')
+## Important Paths
+PROJECT      = os.path.abspath(os.path.dirname(__file__))
+REQUIRE_PATH = "requirements.txt"
+VERSION_PATH = os.path.join(PACKAGE, "version.py")
+PKG_DESCRIBE = "DESCRIPTION.txt"
 
+## Directories to ignore in find_packages
+EXCLUDES     = (
+    "tests", "bin", "docs", "fixtures", "register", "notebooks",
+)
+
+##########################################################################
+## Helper Functions
+##########################################################################
+
+def read(*parts):
+    """
+    Assume UTF-8 encoding and return the contents of the file located at the
+    absolute path from the REPOSITORY joined with *parts.
+    """
+    with codecs.open(os.path.join(PROJECT, *parts), 'rb', 'utf-8') as f:
+        return f.read()
+
+
+def get_version(path=VERSION_PATH):
+    """
+    Reads the __init__.py defined in the VERSION_PATH to find the get_version
+    function, and executes it to ensure that it is loaded correctly.
+    """
+    namespace = {}
+    exec(read(path), namespace)
+    return namespace['get_version']()
+
+
+def get_requires(path=REQUIRE_PATH):
+    """
+    Yields a generator of requirements as defined by the REQUIRE_PATH which
+    should point to a requirements.txt output by `pip freeze`.
+    """
+    for line in read(path).splitlines():
+        line = line.strip()
+        if line and not line.startswith('#'):
+            yield line
+
+##########################################################################
 ## Define the configuration
+##########################################################################
+
 config = {
-    "name": "baleen",
-    "version": "0.1.0",
-    "description": "An automated ingestion service for blogs to construct a corpus for NLP research.",
-    "license": "MIT",
-    "author": "Benjamin Bengfort",
-    "author_email": "benjamin@bengfort.com",
-    "url": "https://github.com/bbengfort/baleen",
-    "download_url": 'https://github.com/bbengfort/baleen/tarball/v0.1.0',
-    "packages": packages,
-    "install_requires": requires,
-    "classifiers": classifiers,
-    "keywords": keywords,
-    "zip_safe": True,
+    "name": NAME,
+    "version": get_version(),
+    "description": DESCRIPTION,
+    "long_description": read(PKG_DESCRIBE),
+    "license": LICENSE,
+    "author": AUTHOR,
+    "author_email": EMAIL,
+    "maintainer": AUTHOR,
+    "maintainer_email": EMAIL,
+    "url": REPOSITORY,
+    "download_url": "{}/tarball/v{}".format(REPOSITORY, get_version()),
+    "packages": find_packages(where=PROJECT, exclude=EXCLUDES),
+    "install_requires": list(get_requires()),
+    "classifiers": CLASSIFIERS,
+    "keywords": KEYWORDS,
+    "zip_safe": False,
     "scripts": ['bin/baleen'],
 }
 
