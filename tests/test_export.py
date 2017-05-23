679725cf1980623381a1eeaf124029835fb6d6c5
Janet Riley <jriley@janetriley.net>
2017-05-23 07:05:04 -0400

    Tweak comments

--- tests/test_export.py
+++ tests/test_export.py
@@ -252,7 +252,7 @@ class ExportTests(unittest.TestCase):
 
 
 class SanitizeHtmlTests(unittest.TestCase):
-    """ Tests the exporter's sanitize methods """
+    """ Tests the exporter's HTML sanitize methods """
 
     @classmethod
     def setUpClass(cls):
@@ -275,22 +275,22 @@ class SanitizeHtmlTests(unittest.TestCase):
         self.conn = None
 
     def test_sanitize_requires_a_valid_level(self):
-        """  Assert that sanitize_html requires a supported level """
+        """  Sanitize_html requires a supported level """
         with self.assertRaises(ExportError):
             self.exporter.sanitize_html(self.sample_html, "bogus")
 
     def test_sanitize_returns_input_for_level_none(self):
-        """  Assert that sanitize_html returns unmodified input for level None """
+        """  sanitize_html returns unmodified input for level None """
         self.assertEqual(self.exporter.sanitize_html(self.sample_html, None), self.sample_html)
 
     def test_sanitize_raw(self):
-        """  Assert that sanitize raw returns the content as submitted """
+        """  Sanitize level raw returns the content as submitted """
         self.assertEqual(self.exporter.sanitize_html(self.sample_html, RAW), self.sample_html)
 
     def test_sanitize_safe(self):
-        """  Assert that sanitize safe applies Readability and returns the body """
+        """  Sanitize level safe applies Readability and returns the body """
 
-        # Give Readability a simpler HTML sample
+        # Give Readability a simpler HTML sample to keep its parse strategy simple
         sample_html = ('<html>'
                        '<head><script>javascript here</script></head>'
                        '<body>body</body>'
@@ -300,7 +300,7 @@ class SanitizeHtmlTests(unittest.TestCase):
 
     def test_sanitize_text(self):
         """
-        Assert that sanitize text strips HTML tags, removes newlines,
+        Sanitize level text strips HTML tags, removes newlines,
          and converts the html entity ampersand into an ampersand character
         """
         expected = 'body & mind'
