96a86a187cc5eb86bbe3e629f43e11ba2bc95310
DDL Admin on Baleen <admin@districtdatalabs.com>
2017-10-12 17:06:03 +0000

    largest doc script

---
+++ bin/largest_doc.py
@@ -0,0 +1,27 @@
+import bson
+from pymongo import MongoClient
+
+
+def main():
+	connection = connection = MongoClient()
+	db = connection.baleen
+	collection = db.posts
+	col_size = collection.count()
+	print("Found %d documents in baleen:posts", col_size)
+	idx = 1
+	
+	col_sizes = {}
+	for  post in collection.find():
+		print("Item {} of {}".format(idx, col_size))
+		#print(post)
+		#print(post['_id'])
+		#print("{} - {}".format(len(post['content']), post['_id']))
+		col_sizes[post['_id']] = len(post['content'])
+		idx += 1
+
+	print(col_sizes)
+
+	for w in sorted(col_sizes, key=col_sizes.get, reverse=True):
+  		print w, col_sizes[w]
+
+main()
